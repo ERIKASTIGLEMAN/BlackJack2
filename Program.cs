d14b64baf8c8b554a9f76edf92a059fceb2e6e1c
ERIKASTIGLEMAN <69441647+ERIKASTIGLEMAN@users.noreply.github.com>
2020-08-17 09:30:43 -0400

    First attempt at BlackJack2 - Building deck

--- Program.cs
+++ Program.cs
@@ -2,11 +2,64 @@
 
 namespace BlackJack2
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void DisplayBlackJackGreeting()
+
+        {
+            Console.WriteLine("♠︎ ♣︎ ♥︎ ♦︎ ♠︎ ♣︎ ♥︎ ♦︎ ♠︎ ♣︎ ♥︎ ♦︎ ♠︎ ♣︎ ♥︎ ♦︎ ♠︎ ♣︎ ♥︎ ♦︎");
+            Console.WriteLine();
+            Console.WriteLine(" Welcome! Try Your Hand at BlackJack ");
+            Console.WriteLine();
+            Console.WriteLine("♠︎ ♣︎ ♥︎ ♦︎ ♠︎ ♣︎ ♥︎ ♦︎ ♠︎ ♣︎ ♥︎ ♦︎ ♠︎ ♣︎ ♥︎ ♦︎ ♠︎ ♣︎ ♥︎ ♦︎");
+            Console.WriteLine();
+        }
+
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            // recalling Black Jack greeting from method adding to top
+
+            DisplayBlackJackGreeting();
+
+            //  create a deck
+
+            var suits = new list<string>() { "Hearts", "Diamonds", "Spade", "Club" };
+            var face = new list<string>() { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
+
+            var deck = new List<string>();
+
+            for (var index = 0; index < suits.Count(); index++) { for (var numbers = 0; numbers < face.Count(); numbers++)};
+
+
+            Console.WriteLine("We have our deck of cards");
+            System.Threading.Thread.Sleep(1500);
+
+
+            //  shuffle deck
+
+            //  PlayerHand
+
+            //  DealerHand
+
+            //  ask deck for card for players hand 2x
+
+            //  ask deck for card for dealers hand 2x
+
+            //  show the players card and total of hand
+
+            //  create conditions based on Busted, Stand, Hit, BlackJack, Winner
+
+            //  Show Dealers Hand and total value of hand
+
+            //  create conditions based on Busted, Stand, Hit, BlackJack, Winner
+
+            //  
+
+        }
     }
-  }
 }
